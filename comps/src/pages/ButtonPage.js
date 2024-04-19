0c95d6a5a47f77b2e0ebf1ea020c4c751f204c22
Frederico Santos <fsraen@gmail.com>
2024-04-19 12:32:54 +0100

    Desenvolvimento de comps

---
+++ comps/src/pages/ButtonPage.js
@@ -0,0 +1,39 @@
+import Button from "../components/Button";
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
+
+function ButtonPage() {
+    const handleClick = () => {
+        console.log("CLICK!");
+    };
+
+    return (
+        <div>    
+            <div>
+                <Button secondary outline rounded className="mb-5" onClick={handleClick}>
+                    <GoBell />
+                    Click me!!
+                </Button>
+            </div>
+            <div>
+                <Button danger outline>
+                    <GoCloudDownload />
+                    Download me!!
+                </Button>
+            </div>
+            <div>
+                <Button warning>
+                    <GoDatabase />
+                    Go database!!
+                </Button>
+            </div>
+            <div>
+                <Button secondary outline>Click me!!</Button>
+            </div>
+            <div>
+                <Button primary rounded>Click me!!</Button>
+            </div>
+        </div>
+    );
+}
+
+export default ButtonPage;
