2a3558303f422d2f80fbfbcd9dbb0e9b856de2d4
ifredamg <fsraen@gmail.com>
2024-05-20 23:52:00 +0100

    Doing module 18 - Into the World of Reducers

--- comps/src/pages/CounterPage.js
+++ comps/src/pages/CounterPage.js
@@ -2,11 +2,24 @@ import { useReducer } from 'react';
 import Button from '../components/Button';
 import Panel from '../components/Panel';
 
+const INCREMENT_COUNT = 'increment';
+const SET_VALUE_TO_ADD = 'change-value-to-add';
+
 const reducer = (state, action) => {
-    return {
-        ...state,
-        count: state.count + 1
-    };
+    switch (action.type) {
+        case INCREMENT_COUNT:
+            return {
+                ...state,
+                count: state.count + 1
+            };
+        case SET_VALUE_TO_ADD:
+            return {
+                ...state,
+                valueToAdd: action.payload
+            };
+        default:
+            throw new Error('Unexpected action type: ' + action.type);
+    }
 };
 
 function CounterPage({ initialCount }) {
@@ -16,10 +29,13 @@ function CounterPage({ initialCount }) {
         count: initialCount,
         valueToAdd: 0
     });
+    console.log(state);
 
     const increment = () => {
         // setCount(count + 1);
-        dispatch();
+        dispatch({
+            type: INCREMENT_COUNT
+        });
     };
     const decrement = () => {
         // setCount(count - 1);
@@ -28,6 +44,10 @@ function CounterPage({ initialCount }) {
     const handleChange = (event) => {
         const value = parseInt(event.target.value) || 0;
         // setValueToAdd(value);
+        dispatch({
+            type: SET_VALUE_TO_ADD,
+            payload: value
+        });
     };
 
     const handleSubmit = (event) => {
